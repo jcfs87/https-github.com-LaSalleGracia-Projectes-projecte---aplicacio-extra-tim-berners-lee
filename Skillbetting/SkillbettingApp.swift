d9b9c696166ff96af0c4b0777ee599fd080f8476
Juan Carlos <juancarlos.faria@gracia.lasalle.cat>
2023-03-31 16:02:33 +0200

    Initial Commit

---
+++ Skillbetting/SkillbettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillbettingApp.swift
+//  Skillbetting
+//
+//  Created by alumne on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkillbettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
